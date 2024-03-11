fb3b6fcd8b87e0c6bc119552bf445b0716010a8b
tamourgh1u <grindmoney19@gmail.com>
2024-03-11 14:54:57 +0100

    index.php

--- singleton/public/index.php
+++ singleton/public/index.php
@@ -5,3 +5,14 @@ require('../vendor/autoload.php');
 # TODO: Récuperer une instance de Config
 # Afficher une valeur contenu dans config.php
 # Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+
+
+
+use App\Config;
+
+$config = Config::getInstance();
+$value = $config->getValue("db");
+var_dump($value);
+
+$config2 = Config::getInstance();
+var_dump($config === $config2); 
