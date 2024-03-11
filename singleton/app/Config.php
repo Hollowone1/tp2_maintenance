a638d761e9c784dfa66eb2dc381753aa1106aed8
Antoine Stoecklin <astoeck@hotmail.fr>
2024-03-11 14:39:42 +0100

    initialized repository

---
+++ singleton/app/Config.php
@@ -0,0 +1,26 @@
+<?php
+
+# TODO: Créer une classe Config en Singleton
+
+namespace App;
+
+class Config {
+
+    private static $_instance;
+    private $settings;
+
+    public static function getInstance(){
+        if (is_null(self::$_instance)){
+            self::$_instance = new Config();
+        }
+        return self::$_instance;
+    }
+
+    private function __construct(){
+        $this->settings= require(__DIR__."/config/config.php");
+    }
+
+    public function getValue($key){
+        if ($key(self::$settings))
+    }
+}
