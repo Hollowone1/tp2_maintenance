4a21af9297f3a3e53a19935631f728263d20c8df
tamourgh1u <grindmoney19@gmail.com>
2024-03-11 15:25:51 +0100

    interface transport

---
+++ factory/app/Entity/Transports.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Bicycle;
+use App\Entity\Car;
+use App\Entity\Truck;
+
+class TransportationFactory {
+    public static function createVehicle($distance, $weight) {
+        if ($distance < 20) {
+            return new Bicycle(0.05, 'muscle');
+        } elseif ($distance >= 20 && $weight < 20) {
+            return new Car(0.1, 'petrol');
+        } elseif ($distance >= 20 && $weight >= 20 && $weight < 200) {
+            return new Car(0.1, 'petrol');
+        } else {
+            return new Truck(0.2, 'diesel');
+        }
+    }
+}
--- factory/public/index.php
+++ factory/public/index.php
@@ -2,3 +2,4 @@
 require('../vendor/autoload.php');
 
 # Essayer d'utiliser votre factory ici
+
